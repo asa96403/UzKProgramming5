d2cf159264cfec21e7f860eaeb045f3b48519568
aabert <aabert@smail.uni-koeln.de>
2025-05-11 18:50:27 +0200

    implemented Claculator1

--- Homework5/src/hw3/Calculator1.java
+++ Homework5/src/hw3/Calculator1.java
@@ -1,11 +1,24 @@
 package hw3;
 
 public class Calculator1 {
-	public static double divide(double a, double b){
-		return a/b;
+	/**
+	 * changed type of a and b to int as it just returns Infinity for doubles when
+	 * dividing by 0
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double divide(int a, int b) throws ArithmeticException {
+		return a / b;
+	}
+
+	// ...
+	public static void main(String[] args) {
+		try {
+			System.out.println(Calculator1.divide(3, 0));
+		} catch (ArithmeticException e) {
+			System.err.println("Error: You cannot divide by 0!");
 		}
-		//...
-		public static void main(String [] args){
-		System.out.println(Calculator1.divide(3, 0));
-		}
+	}
 }
